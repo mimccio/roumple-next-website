d9e7ef02455bb56e2eb235d192d16af63a73a30e
mimccio <miklmccio@gmail.com>
2023-08-25 11:07:00 +0200

    i18n

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,10 +1,15 @@
+import { useTranslation } from '../i18n'
+
 interface Props {
   params: { lang: string }
 }
 
-export default function Home({ params }: Props) {
+export default async function Home({ params }: Props) {
+  const { t } = await useTranslation(params.lang)
   console.log('params.lang :', params.lang)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">hello world {params.lang} </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>{t('title')}</h1> hello world {params.lang}{' '}
+    </main>
   )
 }
