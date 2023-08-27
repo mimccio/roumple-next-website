a9e49ef7baa4206a66199c55740313cef745172c
mimccio <miklmccio@gmail.com>
2023-08-27 10:10:53 +0200

    better accessibiity 2

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,7 +13,7 @@ export async function Header({ lang }: Props) {
 
   return (
     <header className="fixed left-0 right-0 top-0 z-10 flex h-14 items-center justify-end bg-gray-50 px-6 shadow-md 2xs:justify-between">
-      <Link href="/" className="group hidden items-center gap-4 2xs:flex">
+      <Link href="/" className="group hidden items-center gap-4 2xs:flex" aria-label="home">
         <Logo />
         <h2 className="hidden font-bold text-indigo-300 transition-colors group-hover:text-indigo-400 sm:block">
           Roumple
@@ -22,7 +22,7 @@ export async function Header({ lang }: Props) {
 
       <a
         href={myRoumpleUrl}
-        className="flex items-center rounded-lg border border-indigo-100 bg-indigo-50 px-5 py-1 font-semibold text-indigo-500 transition-colors duration-200 hover:border-indigo-200 hover:bg-indigo-100 hover:text-indigo-600"
+        className="flex items-center rounded-lg border border-indigo-100 bg-indigo-50 px-5 py-1 font-semibold text-indigo-600 transition-colors duration-200 hover:border-indigo-200 hover:bg-indigo-100 hover:text-indigo-700"
       >
         {t('login', { ns: 'nav' })}
       </a>
