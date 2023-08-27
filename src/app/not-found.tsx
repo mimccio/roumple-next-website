76daeaf3368294197dc06592ad8c42680f7d6638
mimccio <miklmccio@gmail.com>
2023-08-27 14:51:13 +0200

    Prevent infinite redirect in case not-found page does not exist

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,16 @@
 // When we can, use this file as not-found page and move it under [lang] dir (https://github.com/vercel/next.js/discussions/12477) and delete the main root layout
+'use client'
 
-import { redirect } from 'next/navigation'
+import { redirect, usePathname } from 'next/navigation'
+
+const notFoundPath = '/not-found'
 
 export default function NotFound() {
-  redirect('/not-found')
+  const pathname = usePathname()
+  // Prevent infinite redirect in case not-found page does not exist
+  if (pathname === notFoundPath) {
+    console.error('not-found page does not exist')
+    redirect('/')
+  }
+  redirect(notFoundPath)
 }
